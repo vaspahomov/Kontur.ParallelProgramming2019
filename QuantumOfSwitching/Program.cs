4ffbaae792e8b59e5ab51eee7f096402f454f000
vaspahomov <vas2142553@gmail.com>
2019-03-24 14:38:48 +0500

    task 1

--- QuantumOfSwitching/Program.cs
+++ QuantumOfSwitching/Program.cs
@@ -1,21 +1,54 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FindTimeQuant
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var processorNum = args.Length > 0 ? int.Parse(args[0]) - 1 : 0;
-			Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)(1 << processorNum);
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var processorNum = args.Length > 0 ? int.Parse(args[0]) - 1 : 0;
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr) (1 << processorNum);
 
-			
-		}
-	}
+            var results = new List<long>();
+            var currentThread = Thread.CurrentThread;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var thread1 = new Thread(() =>
+            {
+                for (var i = 0; i < 1000000000; i++)
+                {
+                    if (currentThread == Thread.CurrentThread) continue;
+                    results.Add(stopwatch.ElapsedMilliseconds);
+                    //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                    currentThread = Thread.CurrentThread;
+                    stopwatch.Restart();
+                }
+            });
+
+            var thread2 = new Thread(() =>
+            {
+                for (var i = 0; i < 1000000000; i++)
+                {
+                    if (currentThread == Thread.CurrentThread) continue;
+                    results.Add(stopwatch.ElapsedMilliseconds);
+                    //Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                    currentThread = Thread.CurrentThread;
+                    stopwatch.Restart();
+                }
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine($"Elapsed quantum of switching: {results.Sum() / results.Count}");
+        }
+    }
 }
