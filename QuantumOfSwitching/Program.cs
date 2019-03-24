543e30df167b21915847f0c2d2e826aff28cc193
vaspahomov <vas2142553@gmail.com>
2019-03-24 14:50:56 +0500

    task 2

--- QuantumOfSwitching/Program.cs
+++ QuantumOfSwitching/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
+using System.Windows.Markup;
 
 namespace FindTimeQuant
 {
@@ -11,13 +12,22 @@ namespace FindTimeQuant
         private static void Main(string[] args)
         {
             var processorNum = args.Length > 0 ? int.Parse(args[0]) - 1 : 0;
-            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr) (1 << processorNum);
+            //Process.GetCurrentProcess().ProcessorAffinity = (IntPtr) (1 << processorNum);
+            var proc = (IntPtr) (1 << processorNum);
+            //FindTimeQuant(proc);
+            //Console.WriteLine($"Elapsed quantum of switching: {FindTimeQuant(proc)}");
+            Console.WriteLine($"Count of threads started in 1 second {GetCountOfCreatedThreadsInSecond()}");
 
+    }
+
+        public static long FindTimeQuant(IntPtr proc)
+        {
+            Process.GetCurrentProcess().ProcessorAffinity = proc;
             var results = new List<long>();
             var currentThread = Thread.CurrentThread;
             var stopwatch = new Stopwatch();
             stopwatch.Start();
-
+            
             var thread1 = new Thread(() =>
             {
                 for (var i = 0; i < 1000000000; i++)
@@ -47,8 +57,29 @@ namespace FindTimeQuant
 
             thread1.Join();
             thread2.Join();
+            
+            return results.Sum() / results.Count;
+        }
 
-            Console.WriteLine($"Elapsed quantum of switching: {results.Sum() / results.Count}");
+        public static int GetCountOfCreatedThreadsInSecond()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var count = 0;
+            while (stopwatch.ElapsedMilliseconds < 1000)
+            {
+                
+                var thread = new Thread(() =>
+                {
+                    var x = 1;
+                    Thread.Sleep(1000);
+                });
+                thread.Start();
+                count++;
+            }
+
+            return count;
         }
     }
 }
